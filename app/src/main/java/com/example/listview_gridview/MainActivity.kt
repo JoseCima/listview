9f419de8d11e783410d20552d8100ae1a929b016
JoseCima <josegcima@gmail.com>
2022-03-02 13:03:18 -0500

    List View con adaptador personalizado

--- app/src/main/java/com/example/listview_gridview/MainActivity.kt
+++ app/src/main/java/com/example/listview_gridview/MainActivity.kt
@@ -13,23 +13,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        var frutas:ArrayList<String> = ArrayList()
-        frutas.add("Manzana")
-        frutas.add("Durazno")
-        frutas.add("Platano")
-        frutas.add("Sandía")
+        var frutas:ArrayList<Fruta> = ArrayList()
+        frutas.add(Fruta("Manzana", R.drawable.manzana))
+        frutas.add(Fruta("Durazno", R.drawable.durazno))
+        frutas.add(Fruta("Platano", R.drawable.platano))
+        frutas.add(Fruta("Sandía", R.drawable.sandia))
 
 
         val lista =findViewById<ListView>(R.id.lista)
 
-        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)//Tiene un elemento por celda. Le decimos que tome los valores de nuestro arreglo de frutas
-
+        //val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)//Tiene un elemento por celda. Le decimos que tome los valores de nuestro arreglo de frutas
+        val adaptador =  Adaptador(this, frutas)
         lista.adapter = adaptador
 
 
         //Escuchando los toques
         lista.onItemClickListener = AdapterView.OnItemClickListener{parent, view, position, id ->
-            Toast.makeText(this, frutas.get(position), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, frutas.get(position).nombre, Toast.LENGTH_SHORT).show()
         }
 
 
